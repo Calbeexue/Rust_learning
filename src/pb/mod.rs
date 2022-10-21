198cd4b7cf3a5088975a8ed86430faddafd42182
Calbeexue <Xavier727@sina.com>
2022-10-21 15:49:09 +0800

    learning

---
+++ src/pb/mod.rs
@@ -0,0 +1,60 @@
+pub mod abi;
+
+use abi::{command_request::RequestData, *};
+
+impl CommandRequest {
+    /// 创建 HSET 命令
+    pub fn new_hset(table: impl Into<String>, key: impl Into<String>, value: Value) -> Self {
+        Self {
+            request_data: Some(RequestData::Hset(Hset {
+                table: table.into(),
+                pair: Some(Kvpair::new(key, value)),
+            })),
+        }
+    }
+    ///创建HGET命令
+    pub fn new_hget(table: impl Into<String>, key: impl Into<String> ) -> Self {
+        Self {
+            request_data: Some(RequestData::Hget(Hget {
+                table: table.into(),
+                key: key.into(),
+            })),
+        }
+    }
+    ///创建HGETALL命令
+    pub fn new_hgetall(table: impl Into<String>) -> Self {
+        Self {
+            request_data: Some(RequestData::Hgetall( {
+                table: table.into(),
+            }))
+        }
+    }
+}
+
+impl Kvpair {
+    /// 创建一个新的 kv pair
+    pub fn new(key: impl Into<String>, value: Value) -> Self {
+        Self {
+            key: key.into(),
+            value: Some(value),
+        }
+    }
+}
+
+/// 从 String 转换成 Value
+impl From<String> for Value {
+    fn from(s: String) -> Self {
+        Self {
+            value: Some(value::Value::String(s)),
+        }
+    }
+}
+
+/// 从 &str 转换成 Value
+impl From<&str> for Value {
+    fn from(s: &str) -> Self {
+        Self {
+            value: Some(value::Value::String(s.into())),
+        }
+    }
+}
